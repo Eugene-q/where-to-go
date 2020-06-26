e84954a622877f496efb0a47d2ac5d2681359ebd
Eugene-q <looka12345@yandex.ru>
2020-06-27 00:16:54 +0300

    add frontend

--- project/settings.py
+++ project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
--- project/urls.py
+++ project/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 from datacenter.map_view import map_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', map_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
