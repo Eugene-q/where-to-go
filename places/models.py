33878900feb79ce0009d46f5b89d5743fa274cc2
Eugene-q <looka12345@yandex.ru>
2020-06-28 22:18:12 +0300

    Add Location model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    title = models.CharField(max_length=300)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    coordinates_lng = models.FloatField()
+    coordinates_lat = models.FloatField()
+    
+    def __str__(self):
+        return self.title
