0566ae2b67b27ba2b7c8ea2ea31c568c12f4ad37
Eugene-q <looka12345@yandex.ru>
2020-07-03 20:17:43 +0300

    Geojson from db

--- places/models.py
+++ places/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from geojson import Feature, Point
 
 class Location(models.Model):
     title = models.CharField(max_length=300)
-    description_short = models.TextField()
-    description_long = models.TextField()
+    place_id = models.CharField(max_length=100, unique=True, default="placeId")
     coordinates_lng = models.FloatField()
     coordinates_lat = models.FloatField()
+    details_url = "{% static 'places/moscow_legends.json'%}"#models.FileField(upload_to='json/moscow_legends.json')
     
     def __str__(self):
         return self.title
         
+    def get_place(self):
+        return Feature(geometry=Point([self.coordinates_lng, self.coordinates_lat]), 
+                       properties={"title": self.title,
+                                              "placeId": self.place_id,
+                                              "detailsUrl": self.details_url,
+                       })
 
 class Image(models.Model):
     title = models.CharField(max_length=100)
--- places/views.py
+++ places/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from .models import Location
+from geojson import FeatureCollection
+#import pprint
 
 def map_view(request):
-    return render(request, 'map.html')
+    features = [location.get_place() for location in Location.objects.all()]
+    context = {
+        "locations" : FeatureCollection(features)
+        }
+    #pprint.pprint(context)
+    return render(request, 'map.html', context)
+
