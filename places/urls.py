81a671eb6cf0e9ce44ee740f50d4a0f2328652ae
Eugene-q <looka12345@yandex.ru>
2020-07-05 01:30:00 +0300

    location page

--- places/urls.py
+++ places/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 from places.views import map_view
+from places.views import location_view
 
 urlpatterns = [
     path('', map_view),
+    path('<place_id>/', location_view),
 ]
--- places/views.py
+++ places/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Location
 from geojson import FeatureCollection
+from django.shortcuts import get_object_or_404
 #import pprint
 
 def map_view(request):
@@ -11,3 +12,10 @@ def map_view(request):
     #pprint.pprint(context)
     return render(request, 'map.html', context)
 
+def location_view(request, place_id):
+    place = get_object_or_404(Location, place_id=place_id)
+    context = {
+        "place_title" : place.title,
+        }
+    return render(request, 'location.html', context)
+    
