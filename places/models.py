fad9866e7c34cc2324ab3805cb6812e7ae876512
Eugene-q <looka12345@yandex.ru>
2020-07-13 00:52:15 +0300

    json data on place page

--- places/models.py
+++ places/models.py
@@ -3,10 +3,12 @@ from geojson import Feature, Point
 
 class Location(models.Model):
     title = models.CharField(max_length=300)
-    place_id = models.CharField(max_length=100, unique=True, default="placeId")
+    place_id = models.CharField(max_length=100, unique=True, default='placeId')
+    description_short = models.TextField(default='Описание')
+    description_long = models.TextField(default='Подробное описание')
     coordinates_lng = models.FloatField()
     coordinates_lat = models.FloatField()
-    details_url = "{% static 'places/moscow_legends.json'%}"#models.FileField(upload_to='json/moscow_legends.json')
+    details_url = "{% static 'places/moscow_legends.json'%}"
     
     def __str__(self):
         return self.title
@@ -14,12 +16,13 @@ class Location(models.Model):
     def get_place(self):
         return Feature(geometry=Point([self.coordinates_lng, self.coordinates_lat]), 
                        properties={"title": self.title,
-                                              "placeId": self.place_id,
-                                              "detailsUrl": self.details_url,
+                                   "placeId": self.place_id,
+                                   "detailsUrl": self.details_url,
                        })
 
 class Image(models.Model):
     title = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, default=0)
     image = models.ImageField()
     
     def __str__(self):
--- places/views.py
+++ places/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render
-from .models import Location
+from .models import Location, Image 
 from geojson import FeatureCollection
 from django.shortcuts import get_object_or_404
-#import pprint
+from django.http import JsonResponse
+import json 
 
 def map_view(request):
     features = [location.get_place() for location in Location.objects.all()]
     context = {
         "locations" : FeatureCollection(features)
         }
-    #pprint.pprint(context)
     return render(request, 'map.html', context)
 
 def location_view(request, place_id):
     place = get_object_or_404(Location, place_id=place_id)
-    context = {
-        "place_title" : place.title,
-        }
-    return render(request, 'location.html', context)
+    images = []
+    for image in Image.objects.filter(location=place.id):
+        images.append(str(image.image.file))
+    print(images)
+    response_data = {
+                'title' : place.title,
+                'imgs' : images,
+                'description_short' : place.description_short,
+                'description_long' : place.description_long,
+                'coordinates' : {
+                            'lat' : place.coordinates_lat,
+                            'lng' : place.coordinates_lng,
+                            }
+                }
+    json_dumps_params = {
+                'ensure_ascii' : False,
+                'indent' : 4,
+                }
+    response = JsonResponse(response_data, json_dumps_params=json_dumps_params)
+    return response 
     
