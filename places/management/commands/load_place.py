94f229cda26682cc7e5c4d6929f1bbffb2186bf0
Eu <www@mail.ru>
2020-08-14 13:03:39 +0300

    place_json loads from github

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -5,27 +5,23 @@ import requests, json, os
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('load_dir_path', type=str)
+        parser.add_argument('load_address', type=str)
     def handle(self, *args, **options):
-        load_dir = options['load_dir_path']
-        for json_filename in os.listdir(load_dir):
-            with open(os.path.join(load_dir, json_filename), 'r') as place_json:
-                place = json.load(place_json)
-            new_location, created = Location.objects.get_or_create(
-                                title=place['title'],
-                                description_short=place['description_short'],
-                                description_long=place['description_long'],
-                                coordinates_lng=place['coordinates']['lng'],
-                                coordinates_lat=place['coordinates']['lat'],
-                                )
-            if created:
-                self.stdout.write(self.style.SUCCESS('{} created sucsessfully'.format(place['title'])))
-                for img_link in place['imgs']:
-                    response = requests.get(img_link)
-                    image_obj = Image.objects.create(location=new_location)
-                    image_content = ContentFile(response.content)
-                    image_name = img_link.rpartition('/')[2]
-                    print('saving', image_name)
-                    image_obj.image.save(image_name, image_content, save=True)
-            else:
-                self.stdout.write(self.style.ERROR('{} already exist in database!'.format(place['title'])))
+        place = json.loads(requests.get(options['load_address']).content)
+        new_location, created = Location.objects.get_or_create(
+                            title=place['title'],
+                            description_short=place['description_short'],
+                            description_long=place['description_long'],
+                            coordinates_lng=place['coordinates']['lng'],
+                            coordinates_lat=place['coordinates']['lat'],
+                            )
+        if created:
+            self.stdout.write(self.style.SUCCESS('{} created sucsessfully'.format(place['title'])))
+            for img_link in place['imgs']:
+                image_obj = Image.objects.create(location=new_location)
+                image_content = ContentFile(requests.get(img_link).content)
+                image_name = img_link.rpartition('/')[2]
+                print('saving', image_name)
+                image_obj.image.save(image_name, image_content, save=True)
+        else:
+            self.stdout.write(self.style.ERROR('{} already exists in database!'.format(place['title'])))
